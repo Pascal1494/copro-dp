5ee49b42ab31a4a73ea756ba0310b644a608d313
Pascal <pascal14.lucas@gmail.com>
2023-06-06 18:21:18 +0200

    Validate Home page

--- public/assets/js/scroll.js
+++ public/assets/js/scroll.js
@@ -1,9 +1,21 @@
 const headerEL = document.querySelector(".navbar");
 
+const tabLinks = document.querySelectorAll(".navbar .nav-links ul li a");
+
 window.addEventListener("scroll", () => {
   if (window.scrollY > 75) {
     headerEL.classList.add("navbar-scrolled");
+    tabLinks.forEach((link) => {
+      if (!link.classList.contains("active")) {
+        link.classList.add("nav-font");
+      }
+    });
   } else {
     headerEL.classList.remove("navbar-scrolled");
+    tabLinks.forEach((link) => {
+      if (!link.classList.contains("active")) {
+        link.classList.remove("nav-font");
+      }
+    });
   }
 });
