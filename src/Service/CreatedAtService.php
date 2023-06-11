1a9174b3159d85cc67d800f7f2ce1f8e8762ac7c
Pascal <pascal14.lucas@gmail.com>
2023-06-11 14:29:25 +0200

    Create dasboard 's Admin

--- src/Service/CreatedAtService.php
+++ src/Service/CreatedAtService.php
@@ -1,4 +1,6 @@
-<?php 
+<?php
+
+use DateTimeImmutable;
 
 class CreatedAtService
 {
--- src/Trait/CreatedAtTrait.php
+++ src/Trait/CreatedAtTrait.php
@@ -1,20 +1,21 @@
-<?php 
+<?php
 
 namespace App\Entity\Trait;
 
+use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
 
 trait CreatedAtTrait
 {
-  #[ORM\Column]
-    private ?\DateTimeImmutable $createdAt = null;
+    #[ORM\Column]
+    private ?DateTimeImmutable $createdAt = null;
 
-    public function getCreatedAt(): ?\DateTimeImmutable
+    public function getCreatedAt(): ?DateTimeImmutable
     {
         return $this->createdAt;
     }
 
-    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    public function setCreatedAt(DateTimeImmutable $createdAt): self
     {
         $this->createdAt = $createdAt;
 
